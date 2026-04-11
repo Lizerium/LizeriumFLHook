7bc1e73d4e78855962d1511e2c55db2b314a8b58
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-11 13:54:49 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-11

--- src/Plugins/Public/fast_start_2010/header.h
+++ src/Plugins/Public/fast_start_2010/header.h
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 апреля 2026 12:33:16
- * Version: 1.0.25
+ * Last Updated: 11 апреля 2026 13:40:26
+ * Version: 1.0.26
  */
 
 #ifndef __STARTUP_CACHE_H__
