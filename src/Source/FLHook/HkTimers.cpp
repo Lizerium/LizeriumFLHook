9a8d80afb3daa140b078b0d2294d4b1207d0f463
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-20 03:22:33 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-19

--- src/Source/FLHook/HkTimers.cpp
+++ src/Source/FLHook/HkTimers.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 18 апреля 2026 14:45:47
- * Version: 1.0.466
+ * Last Updated: 20 апреля 2026 03:08:00
+ * Version: 1.0.467
  */
 
 #include <winsock2.h>
