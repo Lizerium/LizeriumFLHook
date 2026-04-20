c677d3e57f5603d36dc6db41d059f56eb055bd34
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-20 16:37:49 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-20

--- src/Plugins/Public/dronebays/Main.cpp
+++ src/Plugins/Public/dronebays/Main.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 20 апреля 2026 03:08:00
- * Version: 1.0.467
+ * Last Updated: 20 апреля 2026 16:23:13
+ * Version: 1.0.468
  */
 
 // DroneBays for FLHookPlugin
