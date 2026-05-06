3986061cdc73832449a042677dd9845d3a65524a
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-06 10:47:19 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-06

--- src/Plugins/Public/revelation-mod-plugin/Language.cpp
+++ src/Plugins/Public/revelation-mod-plugin/Language.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 05 мая 2026 07:03:06
- * Version: 1.0.483
+ * Last Updated: 06 мая 2026 10:31:24
+ * Version: 1.0.484
  */
 
 #include "Language.h"
