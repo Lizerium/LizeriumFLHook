b1570c92417cec26fe4dd80d653cb645d25204f1
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-08 07:04:26 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-08

--- src/Source/FLHook/HkFuncPlayers.cpp
+++ src/Source/FLHook/HkFuncPlayers.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 мая 2026 15:28:52
- * Version: 1.0.485
+ * Last Updated: 08 мая 2026 06:53:21
+ * Version: 1.0.486
  */
 
 #include <fstream>
