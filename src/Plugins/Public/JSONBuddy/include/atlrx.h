4eb5fb08bb5e0d59dd3bb7c3f4a1a26b565a372b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-11 10:54:47 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-11

--- src/Plugins/Public/JSONBuddy/include/atlrx.h
+++ src/Plugins/Public/JSONBuddy/include/atlrx.h
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 мая 2026 07:44:00
- * Version: 1.0.488
+ * Last Updated: 11 мая 2026 10:40:05
+ * Version: 1.0.489
  */
 
 // This is a part of the Active Template Library.
