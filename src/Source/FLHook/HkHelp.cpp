8bc128a04c7824a2331b7d9125740415fed33bf9
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-24 07:09:22 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-24

--- src/Source/FLHook/HkHelp.cpp
+++ src/Source/FLHook/HkHelp.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 23 мая 2026 11:15:14
- * Version: 1.0.500
+ * Last Updated: 24 мая 2026 06:54:03
+ * Version: 1.0.501
  */
 
 #include "Hook.h"
