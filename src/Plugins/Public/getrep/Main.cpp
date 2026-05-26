da8999c6e3eba91924166818c72cf0f43848010f
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-26 12:00:24 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-26

--- src/Plugins/Public/getrep/Main.cpp
+++ src/Plugins/Public/getrep/Main.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 25 мая 2026 11:14:30
- * Version: 1.0.502
+ * Last Updated: 26 мая 2026 11:44:55
+ * Version: 1.0.503
  */
 
 // Rep Plugin - Get ship rep to every faction
