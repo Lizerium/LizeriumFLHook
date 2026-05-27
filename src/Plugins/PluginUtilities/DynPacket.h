0c530ac08eecd258af44dd3ef08ddde38558153a
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-27 13:41:17 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-27

--- src/Plugins/PluginUtilities/DynPacket.h
+++ src/Plugins/PluginUtilities/DynPacket.h
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 26 мая 2026 11:44:55
- * Version: 1.0.503
+ * Last Updated: 27 мая 2026 13:27:39
+ * Version: 1.0.504
  */
 
 #include "FLHook.h"
