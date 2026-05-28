83849ad2057a9bd728b5df12d3becbdc6edf7005
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-28 17:48:01 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-28

--- src/Source/HkLocalized.cpp
+++ src/Source/HkLocalized.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 27 мая 2026 13:27:39
- * Version: 1.0.504
+ * Last Updated: 28 мая 2026 17:32:19
+ * Version: 1.0.505
  */
 
 #include "FLHook/Hook.h"
