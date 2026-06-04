fca58dcbff4b32caa578f758ee048a8933d4e8bb
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-04 07:09:25 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-04

--- src/Plugins/Public/revelation-mod-plugin/ShipData.cpp
+++ src/Plugins/Public/revelation-mod-plugin/ShipData.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 03 июня 2026 15:18:21
- * Version: 1.0.511
+ * Last Updated: 04 июня 2026 06:54:05
+ * Version: 1.0.512
  */
 
 #include "ShipData.h"
