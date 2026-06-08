7ad0808ecac5e432ee968e61f0c370eb15b77498
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-08 07:09:15 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-08

--- src/Source/FLHook/global.h
+++ src/Source/FLHook/global.h
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 июня 2026 18:23:21
- * Version: 1.0.515
+ * Last Updated: 08 июня 2026 06:53:58
+ * Version: 1.0.516
  */
 
 #ifndef _GLOBAL_
