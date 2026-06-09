64a62c038ebcd0debc9f200a1e33aa8ab08be46f
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-09 17:09:55 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-09

--- src/Plugins/PluginUtilities/PluginUtilities.cpp
+++ src/Plugins/PluginUtilities/PluginUtilities.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 июня 2026 06:53:58
- * Version: 1.0.516
+ * Last Updated: 09 июня 2026 16:55:07
+ * Version: 1.0.517
  */
 
 // Common utilities for FLHook plugins
