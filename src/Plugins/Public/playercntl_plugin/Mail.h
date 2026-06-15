387789aedebf09e752d206b9942824ff5b78f4dc
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-15 07:09:34 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-15

--- src/Plugins/Public/playercntl_plugin/Mail.h
+++ src/Plugins/Public/playercntl_plugin/Mail.h
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 июня 2026 16:36:53
- * Version: 1.0.522
+ * Last Updated: 15 июня 2026 06:54:06
+ * Version: 1.0.523
  */
 
 #ifndef __MAIL_H__
