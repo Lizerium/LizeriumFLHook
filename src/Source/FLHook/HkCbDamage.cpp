91d1ede5e9745469df2f2e023f1010551886ef57
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-20 07:07:36 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-20

--- src/Source/FLHook/HkCbDamage.cpp
+++ src/Source/FLHook/HkCbDamage.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 19 июня 2026 06:53:58
- * Version: 1.0.527
+ * Last Updated: 20 июня 2026 06:53:48
+ * Version: 1.0.528
  */
 
 #include "hook.h"
