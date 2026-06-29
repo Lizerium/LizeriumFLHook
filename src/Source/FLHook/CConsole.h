46811a78d1bda2176a965a59ef591a3fa0f3f97f
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-29 07:12:59 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-29

--- src/Source/FLHook/CConsole.h
+++ src/Source/FLHook/CConsole.h
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 июня 2026 11:44:50
- * Version: 1.0.536
+ * Last Updated: 29 июня 2026 06:54:50
+ * Version: 1.0.537
  */
 
 #ifndef _CCONSOLE_
