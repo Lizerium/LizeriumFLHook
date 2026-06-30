67fc3943d2ef366c10ba5c985d9f17ca6badcf7e
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-30 07:41:32 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-30

--- src/Plugins/Public/npc_control/minijson_writer.hpp
+++ src/Plugins/Public/npc_control/minijson_writer.hpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 29 июня 2026 06:54:50
- * Version: 1.0.537
+ * Last Updated: 30 июня 2026 07:23:34
+ * Version: 1.0.538
  */
 
 #ifndef MINIJSON_WRITER_H
