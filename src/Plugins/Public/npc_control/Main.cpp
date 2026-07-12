240b7f6d6865b6b36374db762fc697d99121681c
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-12 07:10:12 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-12

--- src/Plugins/Public/npc_control/Main.cpp
+++ src/Plugins/Public/npc_control/Main.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 11 июля 2026 14:02:51
- * Version: 1.0.548
+ * Last Updated: 12 июля 2026 06:54:09
+ * Version: 1.0.549
  */
 
 // NPCs for FLHookPlugin
