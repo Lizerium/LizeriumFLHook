f6844d2e5c35f8983d3d33269918d4570d6eabcc
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-24 11:56:14 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-24

--- src/Plugins/Public/hookext_plugin/headers/FLCoreServer.h
+++ src/Plugins/Public/hookext_plugin/headers/FLCoreServer.h
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 23 июля 2026 12:57:45
- * Version: 1.0.560
+ * Last Updated: 24 июля 2026 11:40:00
+ * Version: 1.0.561
  */
 
 //////////////////////////////////////////////////////////////////////
