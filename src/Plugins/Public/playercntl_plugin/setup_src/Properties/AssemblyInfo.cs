f380a78c73e3d41e06ff04f9b85fe2ec3e336465
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-25 14:47:25 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-25

--- src/Plugins/Public/playercntl_plugin/setup_src/Properties/AssemblyInfo.cs
+++ src/Plugins/Public/playercntl_plugin/setup_src/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 июля 2026 11:40:00
- * Version: 1.0.561
+ * Last Updated: 25 июля 2026 14:31:12
+ * Version: 1.0.562
  */
 
 using System.Reflection;
