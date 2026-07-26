49713bbf82c4c60bc01b8459d8929e4eab35c5b0
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-26 15:29:30 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-26

--- src/Plugins/Public/cloak_kosa/Cloakkosa.cpp
+++ src/Plugins/Public/cloak_kosa/Cloakkosa.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 25 июля 2026 14:31:12
- * Version: 1.0.562
+ * Last Updated: 26 июля 2026 15:13:04
+ * Version: 1.0.563
  */
 
 #include <windows.h>
