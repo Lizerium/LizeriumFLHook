2b4b46135976dd026c995d7ab79606059b6cb5bc
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-27 15:13:55 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-27

--- src/Plugins/Public/hookext_plugin/hookext_exports.h
+++ src/Plugins/Public/hookext_plugin/hookext_exports.h
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 26 июля 2026 15:13:04
- * Version: 1.0.563
+ * Last Updated: 27 июля 2026 14:58:25
+ * Version: 1.0.564
  */
 
 namespace HookExt
