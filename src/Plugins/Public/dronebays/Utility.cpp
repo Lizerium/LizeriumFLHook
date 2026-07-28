168c73b09d87245e27c7020cd8cb36dfd02b8ce1
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-28 10:26:50 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-28

--- src/Plugins/Public/dronebays/Utility.cpp
+++ src/Plugins/Public/dronebays/Utility.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 27 июля 2026 14:58:25
- * Version: 1.0.564
+ * Last Updated: 28 июля 2026 10:11:55
+ * Version: 1.0.565
  */
 
 #include "Main.h"
