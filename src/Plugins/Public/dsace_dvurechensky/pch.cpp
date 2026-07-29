76a755c17ce81de2e4b6f1bfbcbfc51daf0161dc
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-29 15:59:01 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-29

--- src/Plugins/Public/dsace_dvurechensky/pch.cpp
+++ src/Plugins/Public/dsace_dvurechensky/pch.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 июля 2026 10:11:55
- * Version: 1.0.565
+ * Last Updated: 29 июля 2026 15:46:49
+ * Version: 1.0.566
  */
 
 // pch.cpp: файл исходного кода, соответствующий предварительно скомпилированному заголовочному файлу
--- src/Plugins/Public/help_expanded/Main.cpp
+++ src/Plugins/Public/help_expanded/Main.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 июля 2026 10:11:55
- * Version: 1.0.565
+ * Last Updated: 29 июля 2026 15:46:49
+ * Version: 1.0.566
  */
 
 // Expanded Help Menu - Customisable Help Commands
