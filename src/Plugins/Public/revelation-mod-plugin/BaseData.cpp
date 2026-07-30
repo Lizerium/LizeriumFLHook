b1f579713fc043df56bea87d10a0d873b59d3a7c
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-30 07:06:36 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-30

--- src/Plugins/Public/revelation-mod-plugin/BaseData.cpp
+++ src/Plugins/Public/revelation-mod-plugin/BaseData.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 29 июля 2026 15:46:49
- * Version: 1.0.566
+ * Last Updated: 30 июля 2026 06:53:53
+ * Version: 1.0.567
  */
 
 #include "BaseData.h"
