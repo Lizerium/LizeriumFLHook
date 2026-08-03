38974cc1009c624b617f7669118d24ba50cbcdb4
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-03 07:11:01 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-03

--- src/Plugins/Public/help_expanded/Main.cpp
+++ src/Plugins/Public/help_expanded/Main.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 02 августа 2026 06:54:17
- * Version: 1.0.570
+ * Last Updated: 03 августа 2026 06:54:22
+ * Version: 1.0.571
  */
 
 // Expanded Help Menu - Customisable Help Commands
