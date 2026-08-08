cbb5e9d9a6a6a883b2b6096a62640ce784a50f5b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-08 07:10:38 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-08

--- src/Plugins/Public/revelation-mod-plugin/Command.cpp
+++ src/Plugins/Public/revelation-mod-plugin/Command.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 августа 2026 08:25:29
- * Version: 1.0.575
+ * Last Updated: 08 августа 2026 06:54:15
+ * Version: 1.0.576
  */
 
 #include "Command.h"
