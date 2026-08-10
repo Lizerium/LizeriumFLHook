787fb0d937f57108b8edf3468a75001f75793b1b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-10 07:09:24 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-10

--- src/Plugins/Public/revelation-mod-plugin/FactionData.cpp
+++ src/Plugins/Public/revelation-mod-plugin/FactionData.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 августа 2026 15:32:56
- * Version: 1.0.577
+ * Last Updated: 10 августа 2026 06:54:11
+ * Version: 1.0.578
  */
 
 #include "FactionData.h"
