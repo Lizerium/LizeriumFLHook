8e31b86203b44b6834b05e7674e61f3b1530741b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-12 07:10:16 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-12

--- src/Plugins/Public/CLoak_Kosa Plugin2/Cloakkosa.cpp
+++ src/Plugins/Public/CLoak_Kosa Plugin2/Cloakkosa.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 11 августа 2026 06:54:14
- * Version: 1.0.579
+ * Last Updated: 12 августа 2026 06:54:13
+ * Version: 1.0.580
  */
 
 #include <FLHook.h>
