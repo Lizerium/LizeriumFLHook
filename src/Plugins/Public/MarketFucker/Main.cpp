7eaef25fd0dc74aece3af6d96407f24bf914af84
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-13 07:08:20 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-13

--- src/Plugins/Public/MarketFucker/Main.cpp
+++ src/Plugins/Public/MarketFucker/Main.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 12 августа 2026 06:54:13
- * Version: 1.0.580
+ * Last Updated: 13 августа 2026 06:53:58
+ * Version: 1.0.581
  */
 
 // AlleyPlugin for FLHookPlugin
