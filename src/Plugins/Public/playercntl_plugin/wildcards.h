6143de9eededb35f642536fbd30589ce7f2bd2a3
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-24 07:10:56 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-24

--- src/Plugins/Public/playercntl_plugin/wildcards.h
+++ src/Plugins/Public/playercntl_plugin/wildcards.h
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 23 августа 2026 06:54:23
- * Version: 1.0.591
+ * Last Updated: 24 августа 2026 06:54:17
+ * Version: 1.0.592
  */
 
 #ifndef WILDCARD_H_INCLUDED
