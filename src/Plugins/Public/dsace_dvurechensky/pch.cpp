343ce4e065d876ccf5f755794edf309c555e26d6
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-29 07:09:37 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-29

--- src/Plugins/Public/dsace_dvurechensky/pch.cpp
+++ src/Plugins/Public/dsace_dvurechensky/pch.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 августа 2026 06:54:07
- * Version: 1.0.596
+ * Last Updated: 29 августа 2026 06:54:09
+ * Version: 1.0.597
  */
 
 // pch.cpp: файл исходного кода, соответствующий предварительно скомпилированному заголовочному файлу
--- src/Plugins/Public/revelation-mod-plugin/Data.cpp
+++ src/Plugins/Public/revelation-mod-plugin/Data.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 августа 2026 06:54:07
- * Version: 1.0.596
+ * Last Updated: 29 августа 2026 06:54:09
+ * Version: 1.0.597
  */
 
 #include "Data.h"
