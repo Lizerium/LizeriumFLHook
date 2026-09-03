f6e012461c2a4677cbe4356f2d37710c81f0761b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-03 07:34:54 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-03

--- src/Plugins/Public/revelation-mod-plugin/Hooks.cpp
+++ src/Plugins/Public/revelation-mod-plugin/Hooks.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 02 сентября 2026 06:58:58
- * Version: 1.0.600
+ * Last Updated: 03 сентября 2026 07:19:01
+ * Version: 1.0.601
  */
 
 #include "Hooks.h"
